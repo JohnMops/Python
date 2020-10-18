50d5bd6a0eb13b9ab8ed2407434e3d071dbe6fa4
evgenibi <evgenibi@cloudzone.io>
2020-10-18 15:54:49 +0300

    modifying script for read replica rds

--- AWS/Scripts/create_read_replica_rds.py
+++ AWS/Scripts/create_read_replica_rds.py
@@ -1,9 +1,13 @@
 import boto3
+import time
 import pprint
 
 source_db_id    = "arn:aws:rds:us-east-1:161933537528:db:dr-test-db"  # name of the DataBase if in same region, ARN if not
 db_instance_id  = "dr-test-db-readreplica"  # name of the DataBase Instance
-tags            = [{'Key': 'Name', 'Value': 'test-db'}]  # add more with comma seperation
+tags            = [{'Key': 'Department', 'Value': 'RnD'},
+                   {'Key': 'Environment', 'Value': 'ec-prod-dr'},
+                   {'Key': 'Owner', 'Value': 'DevOps'},
+                   {'Key': 'Purpose', 'Value': 'Enterprise production DR RDS'}]  # add more with comma seperation
 iops            = 1000  # if the above is set to io1
 source_region   = "us-east-1"
 storage_type    = "io1"
@@ -13,41 +17,58 @@ multi_az        = True
 public_access   = False
 db_subnet_group = "db replica sg"
 
-choice = int(input("1. Create read replica\n2. Update SSM Parameter\n>"))
-# Because you cant describe and get the endpoint of a DB right after creation state,
-# run this script after the replica is ready and choose the second option
+rds = boto3.client('rds', region_name='us-east-1')
 
-if choice == 1:
-    rds = boto3.client('rds', region_name='us-west-2')
-    response = rds.create_db_instance_read_replica(
-        DBInstanceIdentifier=db_instance_id,
-        SourceDBInstanceIdentifier=source_db_id,
-        MultiAZ=multi_az,
-        Iops=iops,
-        PubliclyAccessible=public_access,
-        Tags=tags,
-        StorageType=storage_type,
-        SourceRegion=source_region,
-        KmsKeyId=kms_id,
-        DBSubnetGroupName=db_subnet_group
-    )
-    print("Creating Database Instance")
-    pprint.pprint(response)
-elif choice == 2:
-    rds = boto3.client('rds', region_name='us-west-2')
+info = rds.describe_db_instances(
+    DBInstanceIdentifier=source_db_id
+)
+
+while info["DBInstances"][0]["DBInstanceStatus"] != "available":
+    print("Waiting for another query")
+    time.sleep(120)
     info = rds.describe_db_instances(
         DBInstanceIdentifier=db_instance_id
     )
-    ssm = boto3.client('ssm', region_name='us-west-2')
-    param = ssm.put_parameter(
-        Name="/ec/DB_HOST",
-        Value=info["DBInstances"][0]["Endpoint"]["Address"],
-        Type='String',
-        Overwrite=True,
+    print("Status is still: " + info["DBInstances"][0]["DBInstanceStatus"])
+
+print("Status is now Available: Creating Read Replica")
+
+rds = boto3.client('rds', region_name='us-west-2')
+response = rds.create_db_instance_read_replica(
+    DBInstanceIdentifier=db_instance_id,
+    SourceDBInstanceIdentifier=source_db_id,
+    MultiAZ=multi_az,
+    Iops=iops,
+    PubliclyAccessible=public_access,
+    Tags=tags,
+    StorageType=storage_type,
+    SourceRegion=source_region,
+    KmsKeyId=kms_id,
+    DBSubnetGroupName=db_subnet_group
+)
+print("Creating Database Instance")
+pprint.pprint(response)
+rds = boto3.client('rds', region_name='us-west-2')
+info = rds.describe_db_instances(
+    DBInstanceIdentifier=db_instance_id
+)
+
+while info["DBInstances"][0]["DBInstanceStatus"] != "available":
+    print("Waiting for another query")
+    time.sleep(120)
+    info = rds.describe_db_instances(
+        DBInstanceIdentifier=db_instance_id
     )
-    print("Updating the SSM Parameter")
-    pprint.pprint(param)
-else:
-    print("Choose a valid option")
+    print("Status is still: " + info["DBInstances"][0]["DBInstanceStatus"])
 
+print("Status is now Available: Pushing new SSM Parameter")
 
+ssm = boto3.client('ssm', region_name='us-west-2')
+param = ssm.put_parameter(
+    Name="/ec/DB_HOST",
+    Value=info["DBInstances"][0]["Endpoint"]["Address"],
+    Type='String',
+    Overwrite=True,
+)
+print("Updating the SSM Parameter")
+pprint.pprint(param)
