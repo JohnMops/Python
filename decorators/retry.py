0b873a58371899e0a4ff0e77215cc84c621be0cc
John Biriukov <evgeni.biriukov@forescout.com>
2024-06-28 04:43:14 +0200

    adding decorators

--- decorators/retry.py
+++ decorators/retry.py
@@ -1,22 +1,28 @@
 import time            
 
 
-def retry(func, retries: int = 2, delay: int | float = 1):
-    def wrapper():
-        for r in range(1, 1 + retries): 
-            try:
-                print(f'Try ({r}): {func.__name__}')
-                return func()
-            except Exception as e:
-                if r == retry:
-                    print(f'Error: {repr(e)}')
-                    break
-                else:
-                    print(f'Error {repr(e)} ->> Retrying...')
-                    time.sleep(delay)
-    return wrapper
+def retry(retries: int = 2, delay: int | float = 1):
+    if retries < 1 :
+        raise ValueError('Invalid value for "retries"')
+    if delay <= 0:
+        raise ValueError('Invalid value for "delay"')
+    def decorator(func):
+        def wrapper():
+            for r in range(1, 1 + retries): 
+                try:
+                    print(f'Try ({r}): {func.__name__}')
+                    return func()
+                except Exception as e:
+                    if r == retry:
+                        print(f'Error: {repr(e)}')
+                        break
+                    else:
+                        print(f'Error {repr(e)} ->> Retrying...')
+                        time.sleep(delay)
+        return wrapper
+    return decorator
 
-@retry
+@retry()
 def connect():
     print("connecting")
     raise Exception("Dummy Exception")
