f9aba66c8515396c0b74f5e5f8a3f1f89c00f70e
John Biriukov <evgeni.biriukov@forescout.com>
2024-06-29 04:07:37 +0200

    fix invalid value condtition

--- decorators/retry.py
+++ decorators/retry.py
@@ -1,34 +1,42 @@
-import time            
+import time
 
 
 def retry(retries: int = 2, delay: int | float = 1):
-    if retries < 1 :
+    if retries <= 1:
         raise ValueError('Invalid value for "retries"')
     if delay <= 0:
         raise ValueError('Invalid value for "delay"')
+
     def decorator(func):
         def wrapper():
-            for r in range(1, 1 + retries): 
+
+            for r in range(1, 1 + retries):
+
                 try:
-                    print(f'Try ({r}): {func.__name__}')
+                    print(f"Try ({r}): {func.__name__}")
                     return func()
                 except Exception as e:
                     if r == retry:
-                        print(f'Error: {repr(e)}')
+                        print(f"Error: {repr(e)}")
                         break
                     else:
-                        print(f'Error {repr(e)} ->> Retrying...')
+                        print(f"Error {repr(e)} ->> Retrying...")
                         time.sleep(delay)
+
         return wrapper
+
     return decorator
+
 
 @retry()
 def connect():
     print("connecting")
     raise Exception("Dummy Exception")
 
+
 def main():
     connect()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
